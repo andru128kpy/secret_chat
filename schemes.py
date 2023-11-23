bbcdf2b955d9577d597000d606d29c124b3c21b4
andru128kpy <andru128k@gmail.com>
2023-11-23 20:06:31 +0300

    Initial commit

---
+++ schemes.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+
+class Note(BaseModel):
+    text: str
+    secret: str
+    note_hash: str = None
+
+
+class NotesList(BaseModel):
+    all_notes: list[Note] = list()
+
+
+class NoteID(BaseModel):
+    note_id: str
+    note_secret: str
